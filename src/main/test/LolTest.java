1f69d2850a5616f74c1eb8d34a3c6173a540ebbf
alimranbay <alimranbay@gmail.com>
2016-02-09 14:17:40 +0100

    test

---
+++ src/main/test/LolTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class LolTest {
+
+//    @Test
+//    public void aTest() {
+//        assertEquals("test", "test", "");
+//    }
+}
